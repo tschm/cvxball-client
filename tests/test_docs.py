00176b4e15bdf3f0f2124e98d9b20f248618a541
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-23 17:42:29 +0400

    construct points in README

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,5 +1,4 @@
 import doctest
-import os
 import re
 
 import pytest
@@ -22,9 +21,7 @@ def docstring(root_dir):
     return docstring
 
 
-def test_blocks(root_dir, docstring, capfd):
-    os.chdir(root_dir)
-
+def test_blocks(docstring, capfd):
     try:
         doctest.run_docstring_examples(docstring, globals())
     except doctest.DocTestFailure as e:
